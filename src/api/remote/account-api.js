9e96d0a8e839bf1516319d01b874e720ec948d6d
icyfenix <icyfenix@gmail.com>
2020-03-14 23:18:50 +0800

    Merge remote-tracking branch 'origin/master'

--- src/api/remote/account-api.js
+++ src/api/remote/account-api.js
@@ -7,7 +7,7 @@ export default {
    * 根据用户名查询用户信息
    */
   getAccountByUsername (username) {
-    return axios.get(`/account/${username}`)
+    return axios.get(`/accounts/${username}`)
   },
 
   /**
@@ -15,7 +15,7 @@ export default {
    */
   registerAccount (account) {
     // 构造新的用户提交，避免影响界面显示
-    return axios.post('/account', {
+    return axios.post('/accounts', {
       ...account,
       password: api.encrypt.defaultEncode(account.password)
     })
@@ -25,6 +25,6 @@ export default {
    * 更新用户信息
    */
   updateAccount (account) {
-    return axios.put('/account', account)
+    return axios.put('/accounts', account)
   }
 }
--- src/api/remote/payment-api.js
+++ src/api/remote/payment-api.js
@@ -6,7 +6,7 @@ export default {
    * 服务端会进行库存检查、配种地址检查等校验，如果结果满足的话，会执行该结算单，返回订单号和支付二维码
    */
   submitSettlement (settlement) {
-    return axios.post('/settlement', settlement)
+    return axios.post('/settlements', settlement)
   },
 
   /**
